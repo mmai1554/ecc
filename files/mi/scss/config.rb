ddcac203ef7d6c94cee7ad8f8f5edc8afbbacdb2
Michael Mai <mm@mai-internet.de>
2017-09-18 16:53:01 +0200

    before launch

--- files/mi/scss/config.rb
+++ files/mi/scss/config.rb
@@ -19,7 +19,7 @@ sourcemap = true
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
-output_style = :compact
+output_style = :compressed
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 line_comments = false
