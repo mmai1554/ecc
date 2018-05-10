8a3836e4242c378e89a0f405df27c75380b1cff2
Michael Mai <mm@mai-internet.de>
2018-05-10 11:45:23 +0200

    implement carousels for ke

---
+++ templates/ke/carousel_kunden.php
@@ -0,0 +1,161 @@
+<?php
+
+$id_of_carousel = 'carousel_kunden';
+
+?>
+<h2 class="text-center">Kerstins Kunden (Auszug)</h2>
+<div id="<?php echo( $id_of_carousel ); ?>" class="carousel slide mi-carousel-legendaer" data-ride="carousel" data-interval="false">
+    <!-- Indicators -->
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner" role="listbox">
+
+
+        <div class="item active">
+            <div class="mi-card">
+                <img src="/files/kerstin-eisenschmidt/kunden/woman%20speaker%20foundation.png" alt="Woman Speaker Foundation">
+            </div>
+        </div>
+
+        <div class="item">
+            <div class="mi-card">
+                <img src="/files/kerstin-eisenschmidt/kunden/Logo_Weleda.png" alt="Weleda">
+            </div>
+        </div>
+
+        <div class="item">
+            <div class="mi-card">
+                <img src="/files/kerstin-eisenschmidt/kunden/Airbus_Logo.png" alt="Airbus">
+            </div>
+        </div>
+
+        <div class="item">
+            <div class="mi-card">
+                <img src="/files/kerstin-eisenschmidt/kunden/Ak-Hessen.png" alt="AK Hessen">
+            </div>
+        </div>
+
+        <div class="item">
+            <div class="mi-card">
+                <img src="/files/kerstin-eisenschmidt/kunden/Arup.png" alt="Arup">
+            </div>
+        </div>
+
+        <div class="item">
+            <div class="mi-card">
+                <img src="/files/kerstin-eisenschmidt/kunden/Aurelia-Logo.png" alt="Aurelia">
+            </div>
+        </div>
+
+        <div class="item">
+            <div class="mi-card">
+                <img src="/files/kerstin-eisenschmidt/kunden/bbl-mv-logo-2-transparent.png" alt="BBL MV">
+            </div>
+        </div>
+
+        <div class="item">
+            <div class="mi-card">
+                <img src="/files/kerstin-eisenschmidt/kunden/CAU_Logo.png" alt="CAU Kiel">
+            </div>
+        </div>
+
+        <div class="item">
+            <div class="mi-card">
+                <img src="/files/kerstin-eisenschmidt/kunden/Foerde%20Sparkasse.png" alt="Fördesparkasse">
+            </div>
+        </div>
+
+        <div class="item">
+            <div class="mi-card">
+                <img src="/files/kerstin-eisenschmidt/kunden/csm_dampsoft_logo.png" alt="CSM Dampsoft">
+            </div>
+        </div>
+
+        <div class="item">
+            <div class="mi-card">
+                <img src="/files/kerstin-eisenschmidt/kunden/dca.png" alt="DCA">
+            </div>
+        </div>
+
+        <div class="item">
+            <div class="mi-card">
+                <img src="/files/kerstin-eisenschmidt/kunden/GMSH-Logo_a_4c.png" alt="GMSH">
+            </div>
+        </div>
+
+        <div class="item">
+            <div class="mi-card">
+                <img src="/files/kerstin-eisenschmidt/kunden/hansewasser.png" alt="Hansewasser">
+            </div>
+        </div>
+
+        <div class="item">
+            <div class="mi-card">
+                <img src="/files/kerstin-eisenschmidt/kunden/IBSH.png" alt="IBSH">
+            </div>
+        </div>
+
+        <div class="item">
+            <div class="mi-card">
+                <img src="/files/kerstin-eisenschmidt/kunden/KJSH.png" alt="KJSH">
+            </div>
+        </div>
+
+        <div class="item">
+            <div class="mi-card">
+                <img src="/files/kerstin-eisenschmidt/kunden/kreis_nordfriesland.png" alt="Kreis Nordfriesland">
+            </div>
+        </div>
+
+        <div class="item">
+            <div class="mi-card">
+                <img src="/files/kerstin-eisenschmidt/kunden/Lufthansa-Logo.png" alt="Lufthansa Logo">
+            </div>
+        </div>
+
+        <div class="item">
+            <div class="mi-card">
+                <img src="/files/kerstin-eisenschmidt/kunden/S-Servicepartner-Gruppe.png" alt="Sparkasse Servicepartner Gruppe">
+            </div>
+        </div>
+
+        <div class="item">
+            <div class="mi-card">
+                <img src="/files/kerstin-eisenschmidt/kunden/sauerbruch_hutton.png" alt="Sauerbruch Hutton">
+            </div>
+        </div>
+
+        <div class="item">
+            <div class="mi-card">
+                <img src="/files/kerstin-eisenschmidt/kunden/Serviceplus_Logo.png" alt="Service Plus">
+            </div>
+        </div>
+
+        <div class="item">
+            <div class="mi-card">
+                <img src="/files/kerstin-eisenschmidt/kunden/Staab.png" alt="Staab">
+            </div>
+        </div>
+
+        <div class="item">
+            <div class="mi-card">
+                <img src="/files/kerstin-eisenschmidt/kunden/Stadtwerke%20Geesthacht.png" alt="Stadtwerke Geesthacht">
+            </div>
+        </div>
+
+
+
+    </div>
+
+    <!-- Controls -->
+
+    <a class="left carousel-control" href="#<?php echo( $id_of_carousel ); ?>" role="button" data-slide="prev">
+        <div class="mi-carousel-control prev"></div>
+        <span class="sr-only">Previous</span>
+    </a>
+
+    <a class="right carousel-control" href="#<?php echo( $id_of_carousel ); ?>" role="button" data-slide="next">
+        <div class="mi-carousel-control next"></div>
+        <span class="sr-only">Next</span>
+    </a>
+</div>
---
+++ templates/ke/carousel_legendaer.php
@@ -0,0 +1,69 @@
+<?php
+
+$id_of_carousel = 'carousel_legendaer';
+
+?>
+<h2 class="text-center">Legendär</h2>
+<div id="<?php echo( $id_of_carousel ); ?>" class="carousel slide mi-carousel-legendaer" data-ride="carousel" data-interval="false">
+    <!-- Indicators -->
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner" role="listbox">
+	    <?php // item: ?>
+        <div class="item active">
+            <div class="mi-card">
+                <div class="mi-card-text">
+                    „Im Rahmen unserer Expertenvortragsreihe überzeugte Kerstin Eisenschmidt in der Kieler Kunsthalle mit einer hoch spannenden und absolut aktuellen
+                    Keynote. Aus den Megatrends leitete sie logisch die Entwicklung einer integeren Führungskultur ab und lebte es mit ihrer warmherzigen und kraftvollen
+                    Art auch gleich vor. Lebendig und begeisternd zog sie uns alle in den Bann der digitalen Transformation.“
+                </div>
+                <div class="mi-card-signature">
+                    Imke Täufer-Krebs<br>
+                    Personalentwicklung<br>
+                    Christian-Albrechts-Universität zu Kiel
+                </div>
+            </div>
+        </div>
+	    <?php //  item: ?>
+        <div class="item">
+            <div class="mi-card">
+                <div class="mi-card-text">
+                    „Struktur und Leidenschaft, Empathie und Authentizität – Kerstin Eisenschmidt ist non-konformistisch, motivierend und absolut inspirierend: Mit ihrem
+                    klaren, strategischen Denken gibt sie Impulse in ihr Publikum, die unmittelbar Veränderungsprozesse in Gang setzen. Das gelingt ihr, weil sie mit
+                    großer
+                    Leidenschaft überzeugt und ihr Storytelling mit Praxisbeispielen aus ihrer langjährigen Industrie-Erfahrung das Thema sofort erfahrbar macht.“
+                </div>
+                <div class="mi-card-signature">
+                    Regina Mehler<br>
+                    1ST ROW und WOMEN SPEAKER FOUNDATION
+                </div>
+            </div>
+        </div>
+		<?php //  item: ?>
+        <div class="item">
+            <div class="mi-card">
+                <div class="mi-card-text">
+                    „Kerstin berührt und inspiriert uns seit über 10 Jahren – wirkliche Veränderung im Unternehmen braucht eine Initialzündung, genau das erleben wir in
+                    Kerstins Vorträgen.“
+                </div>
+                <div class="mi-card-signature">
+                    Eva Schad<br>
+                    Unternehmensführung, Personalentwicklung, Qualitätsmanagement<br>
+                    David Chipperfield Architects in Berlin
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Controls -->
+
+    <a class="left carousel-control" href="#<?php echo( $id_of_carousel ); ?>" role="button" data-slide="prev">
+        <div class="mi-carousel-control prev"></div>
+        <span class="sr-only">Previous</span>
+    </a>
+
+    <a class="right carousel-control" href="#<?php echo( $id_of_carousel ); ?>" role="button" data-slide="next">
+        <div class="mi-carousel-control next"></div>
+        <span class="sr-only">Next</span>
+    </a>
+</div>
