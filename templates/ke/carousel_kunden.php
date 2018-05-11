fcec29dd421936bd6a77be03cb2a7129628394dc
Michael Mai <mm@mai-internet.de>
2018-05-11 08:51:17 +0200

    add class for customer logos for mobile handling (different as text cards)

--- templates/ke/carousel_kunden.php
+++ templates/ke/carousel_kunden.php
@@ -12,133 +12,133 @@ $id_of_carousel = 'carousel_kunden';
 
 
         <div class="item active">
-            <div class="mi-card">
+            <div class="mi-custlogo">
                 <img src="/files/kerstin-eisenschmidt/kunden/woman%20speaker%20foundation.png" alt="Woman Speaker Foundation">
             </div>
         </div>
 
         <div class="item">
-            <div class="mi-card">
+            <div class="mi-custlogo">
                 <img src="/files/kerstin-eisenschmidt/kunden/Logo_Weleda.png" alt="Weleda">
             </div>
         </div>
 
         <div class="item">
-            <div class="mi-card">
+            <div class="mi-custlogo">
                 <img src="/files/kerstin-eisenschmidt/kunden/Airbus_Logo.png" alt="Airbus">
             </div>
         </div>
 
         <div class="item">
-            <div class="mi-card">
+            <div class="mi-custlogo">
                 <img src="/files/kerstin-eisenschmidt/kunden/Ak-Hessen.png" alt="AK Hessen">
             </div>
         </div>
 
         <div class="item">
-            <div class="mi-card">
+            <div class="mi-custlogo">
                 <img src="/files/kerstin-eisenschmidt/kunden/Arup.png" alt="Arup">
             </div>
         </div>
 
         <div class="item">
-            <div class="mi-card">
+            <div class="mi-custlogo">
                 <img src="/files/kerstin-eisenschmidt/kunden/Aurelia-Logo.png" alt="Aurelia">
             </div>
         </div>
 
         <div class="item">
-            <div class="mi-card">
+            <div class="mi-custlogo">
                 <img src="/files/kerstin-eisenschmidt/kunden/bbl-mv-logo-2-transparent.png" alt="BBL MV">
             </div>
         </div>
 
         <div class="item">
-            <div class="mi-card">
+            <div class="mi-custlogo">
                 <img src="/files/kerstin-eisenschmidt/kunden/CAU_Logo.png" alt="CAU Kiel">
             </div>
         </div>
 
         <div class="item">
-            <div class="mi-card">
+            <div class="mi-custlogo">
                 <img src="/files/kerstin-eisenschmidt/kunden/Foerde%20Sparkasse.png" alt="Fördesparkasse">
             </div>
         </div>
 
         <div class="item">
-            <div class="mi-card">
+            <div class="mi-custlogo">
                 <img src="/files/kerstin-eisenschmidt/kunden/csm_dampsoft_logo.png" alt="CSM Dampsoft">
             </div>
         </div>
 
         <div class="item">
-            <div class="mi-card">
+            <div class="mi-custlogo">
                 <img src="/files/kerstin-eisenschmidt/kunden/dca.png" alt="DCA">
             </div>
         </div>
 
         <div class="item">
-            <div class="mi-card">
+            <div class="mi-custlogo">
                 <img src="/files/kerstin-eisenschmidt/kunden/GMSH-Logo_a_4c.png" alt="GMSH">
             </div>
         </div>
 
         <div class="item">
-            <div class="mi-card">
+            <div class="mi-custlogo">
                 <img src="/files/kerstin-eisenschmidt/kunden/hansewasser.png" alt="Hansewasser">
             </div>
         </div>
 
         <div class="item">
-            <div class="mi-card">
+            <div class="mi-custlogo">
                 <img src="/files/kerstin-eisenschmidt/kunden/IBSH.png" alt="IBSH">
             </div>
         </div>
 
         <div class="item">
-            <div class="mi-card">
+            <div class="mi-custlogo">
                 <img src="/files/kerstin-eisenschmidt/kunden/KJSH.png" alt="KJSH">
             </div>
         </div>
 
         <div class="item">
-            <div class="mi-card">
+            <div class="mi-custlogo">
                 <img src="/files/kerstin-eisenschmidt/kunden/kreis_nordfriesland.png" alt="Kreis Nordfriesland">
             </div>
         </div>
 
         <div class="item">
-            <div class="mi-card">
+            <div class="mi-custlogo">
                 <img src="/files/kerstin-eisenschmidt/kunden/Lufthansa-Logo.png" alt="Lufthansa Logo">
             </div>
         </div>
 
         <div class="item">
-            <div class="mi-card">
+            <div class="mi-custlogo">
                 <img src="/files/kerstin-eisenschmidt/kunden/S-Servicepartner-Gruppe.png" alt="Sparkasse Servicepartner Gruppe">
             </div>
         </div>
 
         <div class="item">
-            <div class="mi-card">
+            <div class="mi-custlogo">
                 <img src="/files/kerstin-eisenschmidt/kunden/sauerbruch_hutton.png" alt="Sauerbruch Hutton">
             </div>
         </div>
 
         <div class="item">
-            <div class="mi-card">
+            <div class="mi-custlogo">
                 <img src="/files/kerstin-eisenschmidt/kunden/Serviceplus_Logo.png" alt="Service Plus">
             </div>
         </div>
 
         <div class="item">
-            <div class="mi-card">
+            <div class="mi-custlogo">
                 <img src="/files/kerstin-eisenschmidt/kunden/Staab.png" alt="Staab">
             </div>
         </div>
 
         <div class="item">
-            <div class="mi-card">
+            <div class="mi-custlogo">
                 <img src="/files/kerstin-eisenschmidt/kunden/Stadtwerke%20Geesthacht.png" alt="Stadtwerke Geesthacht">
             </div>
         </div>
